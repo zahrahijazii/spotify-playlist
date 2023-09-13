eee12f57a37eba01a2b691ab79ebd1ed572ce94b
zahrahijazii <105450781+zahrahijazii@users.noreply.github.com>
2023-09-14 03:04:24 +0400

    Successfully was able to create a playlist with tracks

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import bs4
 import requests 
 import spotipy 
 from spotipy.oauth2 import SpotifyOAuth
+import pprint
 
 
 CLIENT_ID = "294aa54d502041afaa2b3190667a960c"
@@ -11,21 +12,19 @@ REDIRECT_URI = "http://example.com"
 
 #Authentication:
 
-sp_oauth = SpotifyOAuth(
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
     scope="playlist-modify-private",
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
-    redirect_uri=REDIRECT_URI)
+    redirect_uri=REDIRECT_URI,
+    cache_path="C:/Users/HP/Desktop/spotify-playlist/spotify-playlist/.cache",
+    show_dialog=True,
+    username="zahra hijazi"))
 
-token_info = sp_oauth.get_access_token()
 
-if not token_info:
-    auth_url = sp_oauth.get_authorize_url()
-    print(f'Please visit this URL to authorize the application: {auth_url}')
-    response = input('Enter the URL you were redirected to: ')
-    token_info = sp_oauth.get_access_token(response)
+user_id = sp.current_user()["id"]
 
-sp = spotipy.Spotify(auth_manager=token_info["access_token"])
 
 
 user_input = input("What year would you like to travel to? Type the date in this format YYYY-MM-DD: ")
@@ -39,4 +38,37 @@ soup = bs4.BeautifulSoup(billboard_webpage, "html.parser")
 song_list = soup.select("li ul li h3")
 
 song_names = [song_name.getText().strip() for song_name in song_list]
-print(song_names)
+
+song_uris = []
+
+for song_name in song_names:
+    try:
+        uri = sp.search(q=song_name)["tracks"]["items"][1]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song_name} is not found in Spotify. Skipped.")
+
+#create playlist:
+playlist = sp.user_playlist_create(
+    user=user_id, 
+    name=f"{user_input} Billboard 100",
+    public=False
+    )
+
+playlist_id = playlist["id"]
+
+sp.playlist_add_items(
+    playlist_id=playlist_id,
+    items=song_uris
+    )
+
+print("done")
+
+# sp.user_playlist_add_tracks(
+#     user=user_id,
+#     playlist_id=
+
+
+
+
+
